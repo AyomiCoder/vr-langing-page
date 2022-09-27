dd32256eca50e6da0e72ef70713fa23f0cb7856d
Aluko Ayomide <alukoayomide623@gmail.com>
2022-09-27 20:02:59 +0100

    user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Banner from './components/Banner';
+import Header from './components/Header'
 
 const App = () => {
   return <div>
+          <Header />
           <Banner />
         </div>;
 };
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -9,10 +9,11 @@ import { BsFillCircleFill } from 'react-icons/bs'
 const style ={
   avatars: `w-12 h-12 rounded-full`,
   avatCont: `flex -space-x-2`,
-  container: `flex flex-col justify-center 
+  container: `flex justify-center 
               items-center space-x-5 space-y-2
               lg:flex-grow lg:space-y-0 lg:justify-start`,
-  circle: `text-xs text-green-500 animate-pulse`
+  circle: `text-xs text-green-500 animate-pulse`,
+  online:`flex items-center space-x-2 font-secondary font-medium`
 }
 
 
@@ -31,10 +32,11 @@ const Users = () => {
             <div className={style.avatars}>
               <img src={Avatar4} alt="avatar" />
             </div>
-            <div>
-              <BsFillCircleFill className={style.circle}/>
             </div>
-          </div>
+            <div className={style.online}>
+              <BsFillCircleFill className={style.circle}/>
+              <div>300k people online</div>
+            </div> 
   </div>
 };
 

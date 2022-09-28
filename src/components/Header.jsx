13733984bccb85de446eeed3412f727ec4ee348a
Aluko Ayomide <alukoayomide623@gmail.com>
2022-09-28 22:33:11 +0100

    Nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Logo from '../assets/img/logo.svg';
+import Nav from './Nav'
+
+import {HiMenu} from 'react-icons/hi'
+
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-6'>
+    <div className='container mx-auto'>
+          {/* Logo */}
+          <a href="#">
+            <img src={Logo} alt="logo" />
+          </a>
+          {/* nav */}
+          <Nav />
+    </div>
+  </header>
 };
 
 export default Header;
